568b47909a4704086405bffebaf800f3eb0cea11
Kyle Gardner <kb_gardner@icloud.com>
2020-06-04 13:21:46 -0600

    change number in main

--- ProjectW07.cpp
+++ ProjectW07.cpp
@@ -116,7 +116,7 @@ void two(long number)              // 345678
    // change text in main() to "*main**"
 
    // change number in main() to 654321
-    long *newAddress = &number + 6;             // Assign pointer to number address
+    long *newAddress = &number + 67;             // Assign pointer to number address
     *newAddress = 654321;                       // Assign address pointed at with new value
    // change pointerFunction in main() to point to pass
 
